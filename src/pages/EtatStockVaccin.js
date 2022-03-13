365dd3c6eab27e023977a05344c788ab499422e7
Toavina Andria <zotoavina.andria@gmail.com>
2022-03-13 22:28:26 +0300

    modification style modal statistique

--- src/pages/EtatStockVaccin.js
+++ src/pages/EtatStockVaccin.js
@@ -9,7 +9,7 @@ import { etatStockVaccin } from "../mockupData/historique";
 export const EtatStockVaccin = () => {
   const [modalShow, setModalShow] = useState(false);
   const [moyenne, setMoyenne] = useState();
-  const [vaccin, setVaccin]  =  useState();
+  const [vaccin, setVaccin] = useState();
   return (
     <div className="admin-dashboard">
       <Sidebar />
@@ -30,36 +30,40 @@ export const EtatStockVaccin = () => {
               </div>
               <Modal
                 show={modalShow}
-                onHide={()=> setModalShow(false)}
+                onHide={() => setModalShow(false)}
                 size="lg"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
               >
-                <Modal.Header closeButton>
-                  <Modal.Title id="contained-modal-title-vcenter">
-                    <div className="statForm">
-                    <div className="inputGroup">
-                        <label htmlFor="moyenne">Utilisation par semaine</label>
-                        <input type={'text'} id="moyenne" value={moyenne} onChange={(event)=>setMoyenne(event.target.value)}/>
-                    </div>
-                    <div className="inputGroup">
-                        <label htmlFor="listeVaccin">Vaccin</label>
-                        <select id="listeVaccin" onChange={(event)=> setVaccin(event.target.value)}>
-                            <option>{'Vaccin 1'}</option>
-                            <option>{'Vaccin 2'}</option>
-                            <option>{'Vaccin 3'}</option>
-                        </select>
-                    </div>
-                    </div>
-                  </Modal.Title>
-                </Modal.Header>
                 <Modal.Body>
-                  <div className="stat">
-
+                  <div className="statForm">
+                    <div className="inputGroup">
+                      <label htmlFor="moyenne">Utilisation par semaine</label>
+                      <input
+                        type={"number"}
+                        id="moyenne"
+                        value={moyenne}
+                        onChange={(event) => setMoyenne(event.target.value)}
+                      />
+                    </div>
+                    <div className="inputGroup">
+                      <label htmlFor="listeVaccin">Vaccin</label>
+                      <select
+                        id="listeVaccin"
+                        onChange={(event) => setVaccin(event.target.value)}
+                      >
+                        <option>{"Vaccin 1"}</option>
+                        <option>{"Vaccin 2"}</option>
+                        <option>{"Vaccin 3"}</option>
+                      </select>
+                    </div>
                   </div>
+                  <div className="stat"></div>
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button onClick={() => setModalShow(false)} variant="danger">Close</Button>
+                  <Button onClick={() => setModalShow(false)} variant="danger">
+                    Close
+                  </Button>
                 </Modal.Footer>
               </Modal>
               <Table striped bordered hover>
