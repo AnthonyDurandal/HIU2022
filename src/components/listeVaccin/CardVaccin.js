d9d438b7ef25e1343287af59d95260e9df0b1709
TiavinaJoy <joyrajaofetra@gmail.com>
2022-03-13 20:23:53 +0300

    choix vaccin

--- src/components/listeVaccin/CardVaccin.js
+++ src/components/listeVaccin/CardVaccin.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import './../../styles/CentreElement.css'
-import PlanningModal from './PlanningModal';
+import { Modal, Button } from 'react-bootstrap';
+import './../../styles/CardVaccin.css'
 const CardVaccin = (props) => {
     const [modalOption, setModalOption] = useState({ show: false, idCentre: "" })
     const closeModal = () => {
@@ -10,15 +10,16 @@ const CardVaccin = (props) => {
         <div className='element'>
             <div className="meta">
                 <div className="title">
-                    <img src="./img/vaccination.png" alt="" width="50" height="50"></img>
-                    <div className="nom">Vaccin Nom</div>
+                    <img src="./img/vaccination.png" alt="" width="60" height="60"></img>
+                    <div className="nom">{props.data.name}</div>
                 </div>
                 <div className="others">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
-                    <div className="button" onClick={e => { setModalOption({ ...modalOption, show: true }) }}>Reserver</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam.</p>
+                    <Button variant="outline-info">Voir info</Button><br></br><br></br>
+                    <Button variant="success">Choisir</Button>
                 </div>
             </div>
-            <PlanningModal option={modalOption} closeFunction={closeModal} />
+            {/* <PlanningModal option={modalOption} closeFunction={closeModal} /> */}
         </div>
     );
 };
--- src/pages/ChoixVaccin.js
+++ src/pages/ChoixVaccin.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Sidebar from '../components/sidebar/Sidebar';
 import DashboardHeader from '../components/header/DashboardHeader';
-import Carte from '../components/carte/Carte';
-import './../styles/ChoixCentre.css'
+import './../styles/ChoixVaccin.css'
 
-import items from './../data/sidebar.json'
 import clientsidebar from './../data/clientSidebar.json'
-import CentreElement from '../components/choixCentre/CentreElement';
+import CardVaccin from '../components/listeVaccin/CardVaccin';
 const ChoixVaccin = () => {
-    const liste = [{ name: "nom" }, { name: "nom" }]
+    const liste = [{ name: "AstraZeneca" }, { name: "AstraZeneca" }, { name: "AstraZeneca" }, { name: "AstraZeneca" }, { name: "AstraZeneca" }]
     return (
         <div className='admin-dashboard'>
             <Sidebar data={clientsidebar} />
             <DashboardHeader /> {/* top bar */}
+               
             <div className='content'>
-               { liste.map(function (item , index) {
-                   return(<></>)
-               })}
+            <div className='titre'>Liste des vaccin disponible</div>
+              <div className="liste">
+                    {liste.map(function (item, index) {
+                        return (
+                            <CardVaccin data={item} />
+                        )
+                    })}
+              </div>
             </div>
         </div>
     );
